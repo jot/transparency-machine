2608235379232c94ddc448143ee4b703a603b152
Jonathan Markwell <j.l.markwell@inuda.com>
2009-03-07 17:57:34 +0000

    joined home to search

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '82830c718c8629e193d55a8a482322c9'
+  #protect_from_forgery # :secret => '82830c718c8629e193d55a8a482322c9'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
